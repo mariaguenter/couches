048334481aef088ec7bc86a1076627c2f184b2cb
Mathew Levasseur <matlev91@gmail.com>
2017-03-31 17:22:47 -0700

    Include database switching between localhost and server

--- connection.php
+++ connection.php
@@ -1,9 +1,20 @@
 <?php   //Stolen from lab 9 -- need to change it
 
-  $host = "localhost";
-  $database = "couches";
-  $user = "root";
-  $password = "cats";
+  $localhosts = array("::1", "localhost", "127.0.0.1", "192.168.0.1", "192.168.0.0");
+  $hostname = $_SERVER['REMOTE_ADDR'];
+
+  if (in_array($hostname, $localhosts)) {
+    $host = "localhost";
+    $database = "lab8";
+    $user = "root";
+    $password = "";
+  }
+  else {
+    $host = "localhost";
+    $database = "db_33354144";
+    $user = "33354144";
+    $password = "33354144";
+  }
 
   $connection = mysqli_connect($host, $user, $password, $database);
 
